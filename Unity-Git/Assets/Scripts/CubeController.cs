99f59fa5808a92ee16268fff1cd5c597ae2cf0be
xgq <615531676@qq.com>
2017-07-02 18:06:45 +0800

    add CubeController.cs Script

---
+++ Unity-Git/Assets/Scripts/CubeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+
+    }
+
+    void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("<color=darkblue>Key>>>A</color>");
+        }
+    }
+}
